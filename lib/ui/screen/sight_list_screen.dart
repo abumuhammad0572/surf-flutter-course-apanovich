8d5916b86b8dd51838867978f37cbd08201ef9bb
Abu Muhammad <abumuhammad0572@gmail.com>
2022-09-30 10:10:25 +0300

    L-4-4-2

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,8 +10,44 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white10,
+        elevation: 0,
+        toolbarHeight: 80,
+        title: RichText(
+          text: const TextSpan(
+            text: 'С',
+            style: TextStyle(
+              color: Color(0xFF4CAF50),
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.bold,
+              fontSize: 36,
+            ),
+            children: [
+              TextSpan(
+                text: 'писок \n',
+                style: TextStyle(
+                  color: Color(0xFF3B3E5B),
+                ),
+              ),
+              TextSpan(
+                text: 'и',
+                style: TextStyle(
+                  color: Color(0xFFFCDD3D),
+                ),
+              ),
+              TextSpan(
+                text: 'нтересных мест',
+                style: TextStyle(
+                  color: Color(0xFF3B3E5B),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: const Center(
         child: Text('Hello!'),
       ),
     );
